8e7e88c96b53fbc9080ad2500bad42faf629c697
akshayveer <akshayveer096@gmail.com>
2017-07-03 21:48:04 +0530

    Build layout Add single source of truth i.e textarea value to root App component Add some styles for the App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {Grid, Row, Col, FormGroup, FormControl, Button} from 'react-bootstrap'
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {value : ""};
+  }
+  handleChange(e){
+    const val = e.target.val;
+    this.setState({value : val});
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Markdown Preview</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid>
+          <Row>
+            <Col sm={6}>
+              <div id="source">
+                <FormGroup controlId="formControlsTextarea">
+                  <FormControl componentClass="textarea" value={this.state.value} onChange={(e) => this.handleChange(e)}/>
+                </FormGroup>
+              </div>
+            </Col>
+            <Col sm={6}>
+              <div id="preview">
+                <FormGroup controlId="formControlsTextarea">
+                  <FormControl componentClass="textarea" disabled={"disabled"}/>
+                </FormGroup>
+              </div>
+            </Col>
+          </Row>
+          <Button bsStyle="info">Convert</Button>
+        </Grid>
+
       </div>
     );
   }
